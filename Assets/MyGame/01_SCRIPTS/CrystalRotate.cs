c348addda6de590583173cdeb5fa4284d13ee1c9
kycrielos <60536081+kycrielos@users.noreply.github.com>
2021-03-03 18:20:05 +0100

    Cystal

--- Assets/MyGame/01_SCRIPTS/CrystalRotate.cs
+++ Assets/MyGame/01_SCRIPTS/CrystalRotate.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class CrystalRotate : MonoBehaviour
 {
+    private GameObject player;
+    public Transform camTransform;
+    public Cinemachine.CinemachineFreeLook camscript;
+
+    public float angularVelocity;
+
+    private bool activated;
+
+    public float delay;
+    private bool isReady = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        TriggerManager.Activation += ActivationCrystal;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (activated)
+        {
+            float step = angularVelocity * Time.deltaTime;
+            transform.rotation = Quaternion.LookRotation(Vector3.RotateTowards(transform.forward, camTransform.position - transform.position, step, 0));
+            if (Input.GetButtonDown("Interaction") && isReady)
+            {
+                player.SetActive(true);
+                camscript.Priority = 0;
+                isReady = false;
+                player.transform.position = new Vector3(camTransform.position.x, player.transform.position.y, camTransform.position.z);
+                activated = false;
+                StartCoroutine(ActionDelay());
+            }
+        }
+    }
+
+    void ActivationCrystal(GameObject triggerObj)
+    {
+        if (triggerObj.name == gameObject.name + "Trigger" && isReady)
+        {
+            camscript.Priority = 11;
+            isReady = false;
+            player.SetActive(false);
+            activated = true;
+            StartCoroutine(ActionDelay());
+        }
+    }
+
+    IEnumerator ActionDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        isReady = true;
     }
 }
--- Assets/MyGame/01_SCRIPTS/LineRendererController.cs
+++ Assets/MyGame/01_SCRIPTS/LineRendererController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class LineRendererController : MonoBehaviour
 {
+    private Transform crystalTransform;
+    private LineRenderer diagline;
+    private LineRendererController nextCrystalRayon;
+
+    private Vector3 spawnPosition;
+
+    public bool IsActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        diagline = GetComponent<LineRenderer>();
+        crystalTransform = GetComponentInParent<Transform>();
+        spawnPosition = crystalTransform.position + new Vector3(0, 0.2f, 0);
+        diagline.SetPosition(0, spawnPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsActive)
+        {
+            diagline.enabled = true;
+            RaycastHit hit;
+            if (Physics.Raycast(spawnPosition, crystalTransform.forward * -1 + new Vector3(0, 0.1f, 0), out hit, Mathf.Infinity))
+            {
+                diagline.SetPosition(1, hit.point);
+                if (hit.collider.tag == "Crystal")
+                {
+                    nextCrystalRayon = hit.collider.gameObject.GetComponentInChildren<LineRendererController>();
+                    nextCrystalRayon.IsActive = true;
+                }
+                else if (nextCrystalRayon != null)
+                {
+                    nextCrystalRayon.IsActive = false;
+                    nextCrystalRayon.diagline.enabled = false;
+                    nextCrystalRayon = null;
+                }
+            }
+        }
+        else if (nextCrystalRayon != null)
+        {
+            nextCrystalRayon.IsActive = false;
+            nextCrystalRayon.diagline.enabled = false;
+            nextCrystalRayon = null;
+        }
     }
 }
