46bcc147d99d9db2b38bd4fd7f6e18339138be56
Kycrielos <loick.sery@gmail.com>
2021-06-21 09:50:22 +0200

    Platform_Update_and_Fix

--- Assets/MyGame/01_SCRIPTS/PlatformBehaviour.cs
+++ Assets/MyGame/01_SCRIPTS/PlatformBehaviour.cs
@@ -9,33 +9,44 @@ public class PlatformBehaviour : MonoBehaviour
     public bool goPosition1;
     public float speed = 1;
     public float distanceTo;
+    public float delay;
 
     public bool activated;
-    public bool autoMovement;
+    private bool wait;
 
     // Update is called once per frame
     void Update()
     {
-        if (goPosition1)
+        if (!wait)
         {
-            distanceTo = Vector3.Distance(position1.position, transform.position);
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, position1.position, step);
-        }
-        else
-        {
-            distanceTo = Vector3.Distance(position2.position, transform.position);
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, position2.position, step);
-        }
+            if (goPosition1)
+            {
+                distanceTo = Vector3.Distance(position1.position, transform.position);
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, position1.position, step);
+            }
+            else
+            {
+                distanceTo = Vector3.Distance(position2.position, transform.position);
+                float step = speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, position2.position, step);
+            }
 
-        if (distanceTo <= 0.05f && activated)
-        {
-            goPosition1 = !goPosition1;
+            if (distanceTo <= 0.05f && activated)
+            {
+                wait = true;
+                StartCoroutine(DelayPosition());
+            }
         }
-        else if (activated && !autoMovement)
+        /*else if (activated && !autoMovement)
         {
             activated = false;
-        }
+        }*/
+    }
+    private IEnumerator DelayPosition()
+    {
+        yield return new WaitForSeconds(delay);
+        wait = false;
+        goPosition1 = !goPosition1;
     }
 }
